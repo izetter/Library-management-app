1e948b6219a9d285fafea8c78a5be4491d557d0a
izetter <zetter.e@gmail.com>
2020-05-28 14:59:15 -0500

    coming good

--- other.php
+++ other.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library tables</title>
+    <title>OTHER</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -28,7 +28,7 @@
             $table .= "<thead><tr><th>ISBN</th><th>Title</th><th>Publisher</th><th>Year</th><th>Dewey code</th><th>Current inventory</th><th>Total inventory</th></tr></thead>";
             $table .= "<tbody>";
             while($row = $result->fetch_assoc()) {
-                $table .= "<tr>" . "<td>" . $row["ISBN"] . "</td>" . "<td>" . $row["title"] . "<td>" . $row["publisher"] . "</td>" . "<td>" . $row["year"] . "</td>" . "<td>" . $row["dewey_code"] . "</td>" . "<td>" . $row["current_inventory"] . "</td>" . "<td>" . $row["total_inventory"] . "</td>" . "</tr>";
+                $table .= "<tr>" . "<td>" . "<input type='number' value='$row[ISBN]'>" . "</td>" . "<td>" . "<input value='$row[title]'>" . "<td>" . "<input value='$row[publisher]'>" . "</td>" . "<td>" . "<input type='number' value='$row[year]'>" . "</td>" . "<td>" . "<input value='$row[dewey_code]'>" . "</td>" . "<td>" . "<input type='number' value='$row[current_inventory]'>" . "</td>" . "<td>" . "<input type='number' value='$row[total_inventory]'>" . "</td>" . "</tr>";
             }
             $table .= "</tbody>";
             $table .= "</table>";
@@ -47,7 +47,7 @@
             $table .= "<thead><tr><th>Author ID</th><th>Last Name</th><th>First Name</th></tr></thead>";
             $table .= "<tbody>";
             while($row = $result->fetch_assoc()) {
-                $table .= "<tr><td>$row[author_id]</td><td>$row[author_last_name]</td><td>$row[author_first_name]</td></tr>";
+                $table .= "<tr><td><input type='number' value='$row[author_id]'></td><td><input value='$row[author_last_name]'></td><td><input value='$row[author_first_name]'></td></tr>";
             }
             $table .= "</tbody>";
             $table .= "</table>";
@@ -68,7 +68,8 @@
             // output data of each row
             while($row = $result->fetch_assoc()) {
                 // echo "Keyword ID: " . $row["keyword_id"] . " - Keyword: " . $row["keyword"] . "<br>";       // This is how ot was before we implemented the dynamically generated table
-                $table .= "<tr>" . "<td>" . $row["keyword_id"] . "</td>" . "<td>" . $row["keyword"] . "</td>" . "</tr>";
+                // $table .= "<tr>" . "<td>" . $row["keyword_id"] . "</td>" . "<td>" . $row["keyword"] . "</td>" . "</tr>";     // And this is how it was before using inputs
+                $table .= "<tr>" . "<td>" . "<input type='number' value='$row[keyword_id]'>" . "</td>" . "<td>" . "<input value='$row[keyword]'>" . "</td>" . "</tr>";
             }
             $table .= "</tbody>";
             $table .= "</table>";
@@ -76,7 +77,7 @@
         } else {
             echo "0 results";
         }
-        
+
         echo "<br>";
 
         $sql = "SELECT * FROM users";
@@ -84,13 +85,13 @@
         if ($result->num_rows > 0) {
             echo "<h2>Users</h2>";
             $table = "<table>";
-            $table .= "<thead><tr><th>User ID</th><th>email</th><th>Password</th><th>Last name</th><th>First name</th><th>Phone</th><th>Address</th><th>User type</th></tr></thead>";
+            $table .= "<thead><tr><th>User ID</th><th>email</th><th>password</th><th>Last name</th><th>First name</th><th>Phone</th><th>Address</th><th>User type</th></tr></thead>";
             $table .= "<tbody>";
             // output data of each row
             while($row = $result->fetch_assoc()) {
                 // echo "Keyword ID: " . $row["keyword_id"] . " - Keyword: " . $row["keyword"] . "<br>";       // This is how ot was before we implemented the dynamically generated table
                 // $table .= "<tr>" . "<td>" . $row["keyword_id"] . "</td>" . "<td>" . $row["keyword"] . "</td>" . "</tr>";     // And this is how it was before using inputs
-                $table .= "<tr>" . "<td>" . $row["user_id"] . "</td>" . "<td>" . $row["email"] . "</td>" . "<td>" . $row["password"] . "</td>" . "<td>" . $row["user_last_name"] . "</td>" . "<td>" . $row["user_first_name"] . "</td>" . "<td>" . $row["phone"] . "</td>" . "<td>" . $row["address"] . "</td>" . "<td>" . $row["user_type"] . "</td>" . "</tr>";
+                $table .= "<tr>" . "<td>" . "<input type='number' value='$row[user_id]'>" . "</td>" . "<td>" . "<input value='$row[email]'>" . "</td>" . "<td>" . "<input value='$row[password]'>" . "</td>" . "<td>" . "<input value='$row[user_last_name]'>" . "</td>" . "<td>" . "<input value='$row[user_first_name]'>" . "</td>" . "<td>" . "<input value='$row[phone]'>" . "</td>" . "<td>" . "<input value='$row[address]'>" . "</td>" . "<td>" . "<input value='$row[user_type]'>" . "</td>" . "</tr>";
             }
             $table .= "</tbody>";
             $table .= "</table>";
